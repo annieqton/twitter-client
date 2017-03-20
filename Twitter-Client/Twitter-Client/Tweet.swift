8abbef33bb811cdf5a6a7a671d16bf55f4d5b604
Annie Ton-Nu <annieqton@gmail.com>
2017-03-20 11:57:03 -0700

    build Tweet and User

--- Twitter-Client/Twitter-Client/Tweet.swift
+++ Twitter-Client/Twitter-Client/Tweet.swift
@@ -11,25 +11,25 @@ import Foundation
 class Tweet{
     let text: String
     let id: String
-    
+
     var user: User?  //is a var so it can be changed at a later time
-    
+
     //failable initializer (using optionals) that can take ni;
     init?(json: [String: Any]) {
-        
+
         //since we set it as Any type above, we have to type cast it as string, i.e.   as? String
         if let text = json["text"] as? String, let id = json["id_str"] as? String {
-            
+
             self.text = text
             self.id = id
-            
+
             if let userDictionary = json["user"] as? [String: Any] {  //if this fails then we
                 self.user = User(json: userDictionary)               // have a nil user with a valid tweet
             }
-            
+
         } else {
             return nil
         }
     }
-    
+
 }
--- Twitter-Client/Twitter-Client/User.swift
+++ Twitter-Client/Twitter-Client/User.swift
@@ -14,16 +14,16 @@ class User{
     let name: String
     let profileImageURL: String
     let location: String
-    
+
     init?(json: [String: Any]) {
         if let name = json["name"] as? String,
             let profileImageURL = json["profile_image-url"] as? String,
             let location = json["location"] as? String {
-            
+
             self.name = name
             self.profileImageURL = profileImageURL
             self.location  = location
-            
+
         } else {
             return nil
         }
