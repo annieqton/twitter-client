dc07357b7a9236e1af28136b75cb789e2a885217
Annie Ton-Nu <annieqton@gmail.com>
2017-03-23 08:09:08 -0700

    display tweet details and retweet status

---
+++ Twitter-Client/Twitter-Client/Tweetcell.swift
@@ -0,0 +1,15 @@
+//
+//  Tweetcell.swift
+//  Twitter-Client
+//
+//  Created by Annie Ton-Nu on 3/22/17.
+//  Copyright © 2017 Annie Ton-Nu. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var tweetText: UILabel!
+
+}
